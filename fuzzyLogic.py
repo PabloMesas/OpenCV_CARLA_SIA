de4d3ff4d680e9d58fd88586900327d2c8408592
AMendezF <a.mendezworks@gmail.com>
2018-05-05 19:27:07 +0200

    Prepared for test.

--- fuzzyLogic.py
+++ fuzzyLogic.py
@@ -2,57 +2,50 @@ import numpy as np
 import skfuzzy as fuzz
 from skfuzzy import control as ctrl
 
-class fuzzyLogic():
+class FuzzyLogic():
 
-    def __init__ (self, data):
+    def __init__ (self):
+        angleCenter = 26.0
+        angleStep = 0.4
+        angleSigma = 2.5
+        forceCenter = 0.
+        forceStep = 0.35
+        forceSigma = 0.1
+
         # INPUT Variable Angle
-        self.angle = ctrl.Antecedent(np.arange(-90, 90, 0.5), 'angle')
-        self.angle['too_tilted_left'] = fuzz.gaussmf(self.angle.universe, 
-                                                    data['angle']['too_tilted_left'].mean,
-                                                    data['angle']['too_tilted_left'].sigma)
-        self.angle['slightly_tilted_left'] = fuzz.gaussmf(self.angle.universe, 
-                                                    data['angle']['slightly_tilted_left'].mean,
-                                                    data['angle']['slightly_tilted_left'].sigma)
-        self.angle['centered'] = fuzz.gaussmf(self.angle.universe, 
-                                                    data['angle']['centered'].mean,
-                                                    data['angle']['centered'].sigma)
-        self.angle['slightly_tilted_right'] = fuzz.gaussmf(self.angle.universe, 
-                                                    data['angle']['slightly_tilted_right'].mean,
-                                                    data['angle']['slightly_tilted_right'].sigma)
-        self.angle['too_tilted_right'] = fuzz.gaussmf(self.angle.universe, 
-                                                    data['angle']['too_tilted_right'].mean,
-                                                    data['angle']['too_tilted_right'].sigma)
+        self.angle = ctrl.Antecedent(np.arange(0, 90, 0.5), 'angle')
+        self.angle['too_tilted_left'] = fuzz.gaussmf(self.angle.universe, angleCenter * (1 + angleStep * 2), angleSigma)
+        self.angle['slightly_tilted_left'] = fuzz.gaussmf(self.angle.universe, angleCenter * (1 + angleStep), angleSigma)
+        self.angle['centered'] = fuzz.gaussmf(self.angle.universe, angleCenter, angleSigma)
+        self.angle['slightly_tilted_right'] = fuzz.gaussmf(self.angle.universe, angleCenter * (1 - angleStep), angleSigma)
+        self.angle['too_tilted_right'] = fuzz.gaussmf(self.angle.universe, angleCenter * (1 - angleStep * 2), angleSigma)
 
         # OUTPUT Variable Force
-        self.force = ctrl.Consequent(np.arange(-10, 10, 0.1), 'force')
-        self.force['turn_left'] = fuzz.gaussmf(self.force.universe, 
-                                                    data['force']['turn_left'].mean,
-                                                    data['force']['turn_left'].sigma)
-        self.force['slightly_turn_left'] = fuzz.gaussmf(self.force.universe, 
-                                                    data['force']['slightly_turn_left'].mean,
-                                                    data['force']['slightly_turn_left'].sigma)
-        self.force['do_nothing'] = fuzz.gaussmf(self.force.universe, 
-                                                    data['force']['do_nothing'].mean,
-                                                    data['force']['do_nothing'].sigma)
-        self.force['slightly_turn_right'] = fuzz.gaussmf(self.force.universe, 
-                                                    data['force']['slightly_turn_right'].mean,
-                                                    data['force']['slightly_turn_right'].sigma)
-        self.force['turn_right'] = fuzz.gaussmf(self.force.universe, 
-                                                    data['force']['turn_right'].mean,
-                                                    data['force']['turn_right'].sigma)
+        self.force = ctrl.Consequent(np.arange(-1, 1, 0.05), 'force')
+        self.force['turn_left'] = fuzz.gaussmf(self.force.universe, 0 - forceStep * 2, forceSigma)
+        self.force['slightly_turn_left'] = fuzz.gaussmf(self.force.universe, 0 - forceStep, forceSigma)
+        self.force['do_nothing'] = fuzz.gaussmf(self.force.universe, forceCenter, forceSigma)
+        self.force['slightly_turn_right'] = fuzz.gaussmf(self.force.universe, 0 + forceStep, forceSigma)
+        self.force['turn_right'] = fuzz.gaussmf(self.force.universe, 0 + forceStep * 2, forceSigma)
+
         # RULES
-        rule1 = ctrl.Rule(self.angle['too_tilted_left'], self.force['turn_right'])
-        rule2 = ctrl.Rule(self.angle['slightly_tilted_left'], self.force['slightly_turn_right'])
+        rule1 = ctrl.Rule(self.angle['too_tilted_left'], self.force['turn_left'])
+        rule2 = ctrl.Rule(self.angle['slightly_tilted_left'], self.force['slightly_turn_left'])
         rule3 = ctrl.Rule(self.angle['centered'], self.force['do_nothing'])
-        rule4 = ctrl.Rule(self.angle['slightly_tilted_right'], self.force['slightly_turn_left'])
-        rule5 = ctrl.Rule(self.angle['too_tilted_right'], self.force['turn_left'])
+        rule4 = ctrl.Rule(self.angle['slightly_tilted_right'], self.force['slightly_turn_right'])
+        rule5 = ctrl.Rule(self.angle['too_tilted_right'], self.force['turn_right'])
 
         turning_crtl = ctrl.ControlSystem([rule1, rule2, rule3, rule4, rule5])
         self.turning = ctrl.ControlSystemSimulation(turning_crtl)
 
-    def getForce(self, angle):
+    def getForce(self, angle, view=False):
         self.turning.input['angle'] = angle
 
+        # Crunch the numbers
         self.turning.compute()
+
+        if (view == True):
+            self.angle.view(sim=self.turning)
+            self.force.view(sim=self.turning)
         
-        return self.turning.output['force']
+        return round(self.turning.output['force'], 2)
--- trial_fuzzyLogic.py
+++ trial_fuzzyLogic.py
@@ -1,49 +1,7 @@
-import numpy as np
-import skfuzzy as fuzz
-from skfuzzy import control as crtl
+from fuzzyLogic import FuzzyLogic
 
-import msvcrt as m
-def wait():
-    m.getch()
+fuzzy = FuzzyLogic()
+output = fuzzy.getForce(16.0, True)
+print(output)
 
-# New Antecedent/Consecuent objects hold universe variables and membership
-# functions
-quality = crtl.Antecedent(np.arange(0, 11, 1), 'quality')
-service = crtl.Antecedent(np.arange(0, 11, 1), 'service')
-tip = crtl.Consequent(np.arange(0, 26, 1), 'tip')
-
-# Auto-membership function population is possible with .automf(3, 5, or 7)
-quality.automf(3)
-service.automf(3)
-
-# Custom membership functions can be built interactively with a familiar,
-# Pythonic API
-tip['low'] = fuzz.trimf(tip.universe, [0, 0, 13])
-tip['medium'] = fuzz.trimf(tip.universe, [0, 13, 25])
-tip['high'] = fuzz.trimf(tip.universe, [13, 25, 25])
-
-quality['average'].view()
-service.view()
-tip.view()
-
-rule1 = crtl.Rule(quality['poor'] | service['poor'], tip['low'])
-rule2 = crtl.Rule(service['average'], tip['medium'])
-rule3 = crtl.Rule(service['good'] | quality['good'], tip['high'])
-
-rule1.view()
-
-tipping_crtl = crtl.ControlSystem([rule1, rule2, rule3])
-tipping = crtl.ControlSystemSimulation(tipping_crtl)
-
-# Pass inputs to the ControlSystem using Antecedent labels with Pythonic API
-# Note: if you like passinf many inputs all at once, use .inputs(dict_of_data)
-tipping.input['quality'] = 6.5
-tipping.input['service'] = 9.8
-
-# Crunch the numbers
-tipping.compute()
-
-print(tipping.output['tip'])
-tip.view(sim=tipping)
-
-#wait()
+input('Press enter to continue: ')
