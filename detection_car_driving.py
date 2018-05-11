f9ef07d6b72f00dd713fdd37c6b40f9b690aedc0
Leoniels <leonardo.niels@gmail.com>
2018-05-11 14:04:09 +0200

    clustering y cosas

--- detection_car_driving.py
+++ detection_car_driving.py
@@ -1,4 +1,5 @@
 import numpy as np
+import clustering as cl
 from cv2 import cv2 
 
 def get_road_line(frame_RGB):
@@ -28,16 +29,47 @@ def get_road_line(frame_RGB):
     edges = cv2.cvtColor(edges,cv2.COLOR_GRAY2BGR)
     binary = cv2.cvtColor(binary,cv2.COLOR_GRAY2BGR)
     lines_angles = []
-    angles_degrees = []
     try:
             # print (lines_p)
             for line in lines_p: #Print the lines on the Original Frame
                 x1,y1,x2,y2 = line[0] #Points to obtain a line
                 y1 = y1 + 350 #Plus 350 to adapt to the full frame after the crop
                 y2 = y2 + 350
-                angle = get_line_angle(x1,y1,x2,y2)
-                angles_degrees.append(angle)
-                lines_angles.append([[x1,y1,x2,y2], angle]) #Save the angle
+                slope = get_line_slope(x1,y1,x2,y2)
+                displacement = get_line_displacement (x1,y1,x2,y2)
+                line = [slope, displacement]
+                lines_angles.append([line, get_line_angle (slope)]) #Save the angle
+                
+            lines_angles = sorted(lines_angles, key=lambda line: line[1]) # Sorting by angles
+            
+            distances = []
+            jump = 0
+            if abs(lines_angles[0][1] - lines_angles[-1][1]) > 5:
+                sorted_angles = []
+                index = []
+                for pair in lines_angles:
+                   sorted_angles.append(pair[1])
+                index = get_clusters(sorted_angles, 2)
+
+                distance_1 = distance_to_left_margin(lines_angles[0][0][0], lines_angles[0][0][1]) 
+                distance_2 = distance_to_left_margin(lines_angles[index[0]-1][0][0], lines_angles[index[0]-1][0][1])
+                if abs(distance_1) < abs(distance_2):
+                    #First CLuster is Good One
+                    for line in range(0, index[0]-1, 1):
+                        distances.append(distance_to_left_margin(line[0][0], line[0][1]))
+                else:
+                    #Second Cluster is Good One
+                    for line in range(index[0]-1, len(lines_angles), 1):
+                        distances.append(distance_to_left_margin(line[0][0], line[0][1]))
+                    jump = index[0]-1
+                
+            else:
+                for line in lines_angles:
+                    distances.append(line[0][0], line[0][1])  
+            
+            distances.sort()
+            middle_point = len(distances)/2
+            
             
                 cv2.line(frame,(x1,y1),(x2,y2),(255,0,0),2)
                 print_angles_frame(x1, y1, x2, y2, angle, frame) #Print the angles on the frame (for debug)
@@ -49,10 +81,10 @@ def get_road_line(frame_RGB):
         print('En este frame no se detectan líneas por el método probabilístico')
 
     print(lines_angles)
-    return frame, angles_degrees
+    return frame, distances[middle_point], lines_angles[middle_point + jump][1]
 
-def get_line_angle (x1,y1,x2,y2): # Obtain the angle of the line between the point x1,y1 & x2,y2
-    degrees = ((get_line_slope(x1,y1,x2,y2) * 180)/np.pi) #Transform the radians to degrees with 180/pi
+def get_line_angle (slope): # Obtain the angle of the line between the point x1,y1 & x2,y2
+    degrees = ((slope * 180)/np.pi) #Transform the radians to degrees with 180/pi
     return degrees
 
 def get_line_slope (x1,y1,x2,y2): #Get the slope (m) of the line with the line formula y=x*m + c
@@ -79,7 +111,13 @@ def print_angles_frame (x1, y1, x2, y2, angle, frame):
     new_x = (x2-x1) * ((rnd_y-y1)/(y2-y1)) + x1
     cv2.putText(frame, str(np.round(angle,decimals=2)), (new_x + 10, rnd_y), font, 1.5, (0,0,0), 2, cv2.LINE_AA)
 
-def distance_to_left_margin (line): #To get distance we use the point x=0, y=600
-    slope, c = line #Line is a list formed by [slope, displacement] of the line formula y = x*m + c
+def distance_to_left_margin (slope, c): #To get distance we use the point x=0, y=600
     x = (600 - c)/slope
     return x
+
+def get_clusters (angles, k):
+    clustering = cl.Clustering(angles, k)
+    clustering.collect_datos()
+    clustering.procesar_datos()
+    
+    return clustering.get_index_cluster()
